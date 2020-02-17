ca2188a85e7d5813648c0b9a2c9d170da214f539
Szymon <szymon.gruszczynski@roche.com>
2020-02-17 13:14:12 +0100

    Updates

---
+++ precache-manifest.b9b38043c3cafdc2a1372c7f7e94a01f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68696324904c3f32a848844ea786b74b",
+    "url": "/ParcelTracking/index.html"
+  },
+  {
+    "revision": "a6d2b60a040fb713cff5",
+    "url": "/ParcelTracking/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae9eab65fa65a135259f",
+    "url": "/ParcelTracking/static/js/2.81402975.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ParcelTracking/static/js/2.81402975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d2b60a040fb713cff5",
+    "url": "/ParcelTracking/static/js/main.6e1d4cdc.chunk.js"
+  },
+  {
+    "revision": "12444b86582f5d3519f4",
+    "url": "/ParcelTracking/static/js/runtime-main.0a234e96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ParcelTracking/static/media/logo.5d5d9eef.svg"
+  }
+]);
